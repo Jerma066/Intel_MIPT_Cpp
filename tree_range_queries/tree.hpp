b26fa03aa0425e8b1f83cdf357b4cf1775a4c99e
Jerma066 <ragimov.ib@phystech.edu>
2020-10-19 21:07:44 +0300

    Исправление ошибок алгоритма.

--- tree_range_queries/tree.hpp
+++ tree_range_queries/tree.hpp
@@ -1,5 +1,5 @@
 #include <set>
-
+#include <iostream>
 
 namespace trees {
 	
@@ -11,15 +11,21 @@ public:
 	void insertKey(int key) {
 		tree.insert(key);
 	}
+	
 	size_t getRangeQuerieCount(int first, int second) {
-		size_t cnt = 0;
-		for(auto it = tree.lower_bound(first); it != tree.upper_bound(second); it++) {
-			cnt++; 
-		}
-		return cnt;
+		size_t surplus = 0; 
+		
+		tree_iter fi = tree.lower_bound(first);
+		if(*fi == first) {surplus++;}
+		
+		tree_iter si = tree.upper_bound(second);
+		if(*(--si) == second) {surplus++;}
+		
+		return (std::distance(fi, ++si) - surplus);
 	}
 	
 private:
+	using tree_iter = typename std::set<int>::iterator;
 	std::set<int> tree;
 };
 
