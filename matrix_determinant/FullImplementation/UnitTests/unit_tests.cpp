89f6db2119aaf0e28f5be9c8f4ccb2f51ecfbce4
Jerma066 <ragimov.ib@phystech.edu>
2020-11-15 13:31:40 +0300

    Добавление тестов.

--- matrix_determinant/FullImplementation/UnitTests/unit_tests.cpp
+++ matrix_determinant/FullImplementation/UnitTests/unit_tests.cpp
@@ -1,40 +1,7 @@
 #include "test_runner.h"
 #include "../matrix.hpp"
 
-void TestLUDecomposition() {
-	using namespace linalg;
-	{
-		Matrix<double> B({
-			{1, 2, 1}, 
-			{2, 1, 1}, 
-			{1, -1, 2}
-		});	
-		
-		Matrix<double> A({
-			{1, 2, 1}, 
-			{2, 1, 1}, 
-			{1, -1, 2}
-		});		
-		
-		auto[L, U] = A.LUDecomposition();
-		
-		Matrix<double> resL({
-			{1, 0, 0},
-			{2, 1, 0},
-			{1, 1, 1}
-		}); 
-		ASSERT_EQUAL(resL, L);
-		
-		Matrix<double> resU({
-			{1, 2, 1},
-			{0, -3, -1},
-			{0, 0, 2}
-		});
-		ASSERT_EQUAL(resU, U);
-	}
-}
-
-void TestPureDeterminant() {
+void TestMatrixMultiplicaion() {
 	using namespace linalg;
 	{
 		Matrix<double> A({
@@ -43,8 +10,33 @@ void TestPureDeterminant() {
 			{9, 47, 54}
 		});
 		
-		//ASSERT_EQUAL(A.det(), 373);
-		ASSERT_EQUAL(A.LUDet(), 373);
+		Matrix<double> B({
+			{1, 2, 1}, 
+			{2, 1, 1}, 
+			{1, -1, 2}
+		});	
+		
+		Matrix<double> res({
+			{50, 22, 34}, 
+			{24, 3, 23}, 
+			{157, 11, 164}
+		});	
+				
+		ASSERT_EQUAL(A*B, res);
+	}
+	
+}
+
+void TestDeterminant() {
+	using namespace linalg;
+	{
+		Matrix<double> A({
+			{3, 21, 5}, 
+			{1, 8, 7}, 
+			{9, 47, 54}
+		});
+		
+		ASSERT_EQUAL(A.GDet(), 373);
 	}
 	
 	{
@@ -53,8 +45,8 @@ void TestPureDeterminant() {
 			{1, 1}, 
 		});
 		
-		//ASSERT_EQUAL(A.det(), 0);
-		ASSERT_EQUAL(A.LUDet(), 0)
+		ASSERT_EQUAL(A.GDet(), 0);
+		
 	}
 	
 	{
@@ -63,16 +55,17 @@ void TestPureDeterminant() {
 			{0, 1}, 
 		});
 		
-		//ASSERT_EQUAL(A.det(), 1);
-		ASSERT_EQUAL(A.LUDet(), 1)
+		ASSERT_EQUAL(A.GDet(), 1);
+		
 	}
 }
 
 int main() {
 	TestRunner tr;
 
-	RUN_TEST(tr, TestLUDecomposition);
-	RUN_TEST(tr, TestPureDeterminant);
+	//RUN_TEST(tr, TestLUDecomposition);
+	RUN_TEST(tr, TestDeterminant);
+	RUN_TEST(tr, TestMatrixMultiplicaion);
 		
 	return 0;
 }
